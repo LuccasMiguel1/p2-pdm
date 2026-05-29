c18b3e686e47850579e8becbd974f93d067d18ca
LuccasMiguel1 <lumi137@hotmail.com>
2026-05-29 06:40:30 -0300

    Feat(p2): implementa a busca por capital

--- p2-pdm/App.js
+++ p2-pdm/App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View, Pressable, Image } from 'react-native';
 import {useState} from 'react';
 import { FlatList } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
   const [pais, setPais] = useState('');
   const [listaPaises, setListaPaises] = useState([]);
+  const [tipoBusca, setTipoBusca] = useState('name');
   const buscarPais = async () => {
     const resposta = await fetch(
-    `https://restcountries.com/v3.1/name/${pais}`);
+    `https://restcountries.com/v3.1/${tipoBusca}/${pais}`);
     const dados = await resposta.json();
     setListaPaises(dados);
   }
@@ -16,6 +18,30 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+
+      <Picker
+        selectedValue={tipoBusca}
+        style={{
+          width: 200,
+          backgroundColor: 'white',
+          marginBottom: 10
+        }}
+        onValueChange={(itemValue) =>
+          setTipoBusca(itemValue)
+        }
+      >
+
+        <Picker.Item
+          label="Buscar por País"
+          value="name"
+        />
+
+        <Picker.Item
+          label="Buscar por Capital"
+          value="capital"
+        />
+
+      </Picker>
 
       <TextInput
         style={styles.input}
@@ -33,30 +59,52 @@ export default function App() {
         </Text>
       </Pressable>
 
-      <FlatList
-        data={listaPaises}
-          renderItem={({ item }) => (
-            <View>
-              <Text><Text style={{ fontWeight: 'bold' }}>Nome Comum: </Text> 
-              {item.translations.por.common} </Text>
-              <Text><Text style={{ fontWeight: 'bold' }}>Nome Oficial: </Text> 
-              {item.name.official} </Text>
-              <Text><Text style={{ fontWeight: 'bold' }}>Nome em Russo: </Text> 
-              {item.translations.rus.official} </Text>
-              <Text><Text style={{ fontWeight: 'bold' }}>Mapas: </Text> 
-              {item.maps.openStreetMaps} </Text>
-              <Text style={{ fontWeight: 'bold' }}>Bandeira: </Text>
-              <Image
-                source={{ uri: item.flags.png }}
-                style={{
-                  width: 300,
-                  height: 200
-                }}
-              />
-            </View>
-          )}
-          keyExtractor={(item) => item.name.common}
-      />
+      {tipoBusca === 'name' && (
+        <FlatList
+          data={listaPaises}
+            renderItem={({ item }) => (
+              <View>
+                <Text><Text style={{ fontWeight: 'bold' }}>Nome Comum: </Text> 
+                {item.translations.por.common} </Text>
+                <Text><Text style={{ fontWeight: 'bold' }}>Nome Oficial: </Text> 
+                {item.name.official} </Text>
+                <Text><Text style={{ fontWeight: 'bold' }}>Nome em Russo: </Text> 
+                {item.translations.rus.official} </Text>
+                <Text><Text style={{ fontWeight: 'bold' }}>Mapas: </Text> 
+                {item.maps.openStreetMaps} </Text>
+                <Text style={{ fontWeight: 'bold' }}>Bandeira: </Text>
+                <Image
+                  source={{ uri: item.flags.png }}
+                  style={{
+                    width: 300,
+                    height: 200
+                  }}
+                />
+              </View>
+            )}
+            keyExtractor={(item) => item.name.common}
+        />
+      )}
+      {tipoBusca === 'capital' && (
+        <FlatList
+          data={listaPaises}
+            renderItem={({ item }) => (
+              <View>
+                <Text><Text style={{ fontWeight: 'bold' }}>Nome Comum: </Text> 
+                {item.translations.por.common} </Text>
+                <Text style={{ fontWeight: 'bold' }}>Bandeira: </Text>
+                <Image
+                  source={{ uri: item.flags.png }}
+                  style={{
+                    width: 300,
+                    height: 200
+                  }}
+                />
+              </View>
+            )}
+            keyExtractor={(item) => item.name.common}
+        />
+      )}
     </View>
   );
 }
